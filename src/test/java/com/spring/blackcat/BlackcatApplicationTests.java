36231734760ecc8f58be7d12298aeb36d4f51a78
alwayslee <ddoddo1997@naver.com>
2022-09-26 23:46:22 +0900

    Initial commit

---
+++ src/test/java/com/spring/blackcat/BlackcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.blackcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

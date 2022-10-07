b10bd1ef8a172eb16c00554398985ba41f62df64
alwayslee <ddoddo1997@naver.com>
2022-10-07 23:58:39 +0900

    Style: Remove unnecessary code

--- src/test/java/com/spring/blackcat/tattoo/GetTattoosByCategoryIdTest.java
+++ src/test/java/com/spring/blackcat/tattoo/GetTattoosByCategoryIdTest.java
@@ -109,25 +109,4 @@ public class GetTattoosByCategoryIdTest {
         assertThat(tattoos.size()).isEqualTo(5);
         assertThat(tattoos.get(0).getPrice()).isEqualTo(0);
     }
-
-    //InitDB 없애고 사용할 예정(현재 테스트 코드가 InitDB 데이터에 의존적임)
-//    private Long insertAndReturnCategoryId() {
-//        String userName = "Admin1";
-//
-//        Category category = new Category("카테고리1", userName, userName);
-//        categoryRepository.save(category);
-//
-//        Tattoo tattoo1 = new Tattoo("타투1", "설명근", 10000L, 4, category, TattooType.DESIGN, userName, userName);
-//        Tattoo tattoo2 = new Tattoo("타투2", "설명근", 20000L, 4, category, TattooType.DESIGN, userName, userName);
-//        Tattoo tattoo3 = new Tattoo("타투2", "설명근", 20000L, 4, category, TattooType.DESIGN, userName, userName);
-//        Tattoo tattoo4 = new Tattoo("타투2", "설명근", 20000L, 4, category, TattooType.DESIGN, userName, userName);
-//        Tattoo tattoo5 = new Tattoo("타투2", "설명근", 20000L, 4, category, TattooType.DESIGN, userName, userName);
-//        tattooRepository.save(tattoo1);
-//        tattooRepository.save(tattoo2);
-//        tattooRepository.save(tattoo3);
-//        tattooRepository.save(tattoo4);
-//        tattooRepository.save(tattoo5);
-//
-//        return category.getId();
-//    }
 }

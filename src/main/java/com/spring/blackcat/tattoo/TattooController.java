678c26be32f9c0e8e05aede97395266868627905
alwayslee <ddoddo1997@naver.com>
2022-10-07 21:35:25 +0900

    Fix: Set default pageable object

--- src/main/java/com/spring/blackcat/tattoo/TattooController.java
+++ src/main/java/com/spring/blackcat/tattoo/TattooController.java
@@ -3,6 +3,8 @@ package com.spring.blackcat.tattoo;
 import com.spring.blackcat.tattoo.dto.GetAllTattoosResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,7 @@ public class TattooController {
     private final TattooService tattooService;
 
     @GetMapping("/tattoos/{userId}")
-    public List<GetAllTattoosResDto> getAllTattoos(Pageable pageable, @PathVariable("userId") String userId) {
+    public List<GetAllTattoosResDto> getAllTattoos(@PageableDefault(page = 0, size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable, @PathVariable("userId") String userId) {
         return this.tattooService.getAllTattoos(pageable, userId);
     }
 }

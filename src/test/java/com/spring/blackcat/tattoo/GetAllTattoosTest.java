21c25ed3e1da887a72357d22ea90c249c6e587db
alwayslee <ddoddo1997@naver.com>
2022-10-07 22:39:38 +0900

    Test: Add get all tattoos service test code

---
+++ src/test/java/com/spring/blackcat/tattoo/GetAllTattoosTest.java
@@ -0,0 +1,105 @@
+package com.spring.blackcat.tattoo;
+
+import com.spring.blackcat.category.CategoryRepository;
+import com.spring.blackcat.tattoo.dto.GetAllTattoosResDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class GetAllTattoosTest {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    TattooRepository tattooRepository;
+    @Autowired
+    TattooService tattooService;
+
+    @Test
+    @DisplayName("쿼리 스트링 없을 때 전체 타투 조회")
+    void getAllTattoosNoQueryString() {
+        //given
+        String userName = "Admin1";
+        //default pageable object
+        PageRequest pageRequest = PageRequest.of(0, 20, Sort.Direction.DESC, "id");
+
+        //when
+        List<GetAllTattoosResDto> allTattoos = tattooService.getAllTattoos(pageRequest, userName);
+
+        //then
+        assertThat(allTattoos.size()).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("쿼리 스트링 페이지만 넣었을 때 전체 타투 조회")
+    void getAllTattoosOnlyPageQueryString() {
+        //given
+        String userName = "Admin1";
+        //default pageable object
+        PageRequest pageRequest = PageRequest.of(1, 20, Sort.Direction.DESC, "id");
+
+        //when
+        List<GetAllTattoosResDto> allTattoos = tattooService.getAllTattoos(pageRequest, userName);
+
+        //then
+        //기본 20개 씩 조회 시, 두 번째 페이지에는 데이터가 없음(총 데이터 20개)
+        assertThat(allTattoos.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("쿼리 스트링 사이즈만 넣었을 때 전체 타투 조회")
+    void getAllTattoosOnlySizeQueryString() {
+        //given
+        String userName = "Admin1";
+        //default pageable object
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.Direction.DESC, "id");
+
+        //when
+        List<GetAllTattoosResDto> allTattoos = tattooService.getAllTattoos(pageRequest, userName);
+
+        //then
+        assertThat(allTattoos.size()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("쿼리 스트링 페이지, 사이즈만 넣었을 때 전체 타투 조회")
+    void getAllTattoosOnlyPageAndSizeQueryString() {
+        //given
+        String userName = "Admin1";
+        //default pageable object
+        PageRequest pageRequest = PageRequest.of(1, 5, Sort.Direction.DESC, "id");
+
+        //when
+        List<GetAllTattoosResDto> allTattoos = tattooService.getAllTattoos(pageRequest, userName);
+
+        //then
+        assertThat(allTattoos.size()).isEqualTo(5);
+    }
+
+//    @Test
+//    @DisplayName("쿼리 스트링 모두 넣었을 때 전체 타투 조회")
+//    void getAllTattoosAllQueryString() {
+//        //given
+//        String userName = "Admin1";
+//        //default pageable object
+//        PageRequest pageRequest = PageRequest.of(1, 8, Sort.Direction.DESC, "price");
+//
+//
+//        //when
+//        List<GetAllTattoosResDto> allTattoos = tattooService.getAllTattoos(pageRequest, userName);
+//
+//        //then
+//        assertThat(allTattoos.size()).isEqualTo(8);
+//        assertThat(allTattoos.get(0).getPrice())
+//    }
+}

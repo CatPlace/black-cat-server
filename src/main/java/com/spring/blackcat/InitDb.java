a754058c383331849187d798607fba3a0a8ca279
Hyunjik Lee <jiggy0429@gmail.com>
2022-10-10 01:31:19 +0900

    Fix: Modify InitDb.java due to adding new columns

--- src/main/java/com/spring/blackcat/InitDb.java
+++ src/main/java/com/spring/blackcat/InitDb.java
@@ -4,9 +4,7 @@ import com.spring.blackcat.address.Address;
 import com.spring.blackcat.address.AddressRepository;
 import com.spring.blackcat.category.Category;
 import com.spring.blackcat.category.CategoryRepository;
-import com.spring.blackcat.code.CellType;
-import com.spring.blackcat.code.Role;
-import com.spring.blackcat.code.TattooType;
+import com.spring.blackcat.code.*;
 import com.spring.blackcat.likes.LikesService;
 import com.spring.blackcat.magazine.Cell;
 import com.spring.blackcat.magazine.Magazine;
@@ -124,13 +122,13 @@ public class InitDb {
             List<Cell> cellList1 = new ArrayList<>();
             List<Cell> cellList2 = new ArrayList<>();
 
-            cellList1.add(createCell(CellType.GRAYTEXTCELL, "타투 말은 많이 들어보고 주변에서도 요즘 많이 하던데 뭐가 뭔지 잘 모르겠어"));
-            cellList1.add(createCell(CellType.GRAYTEXTCELL, "사람마다 모양, 색깔도 다르던데... 그리고 그거 아픈거 아냐?"));
-            cellList1.add(createCell(CellType.GRAYTEXTCELL, "하는 당신을 위해 타투이스트 깜냥이가 모두 답변해드리겠습니닷!"));
-            cellList1.add(createCell(CellType.IMAGECELL, "https://s3...."));
-            cellList2.add(createCell(CellType.GRAYTEXTCELL, "부위별 타투 통증"));
-            cellList2.add(createCell(CellType.IMAGECELL, "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F90c438c0-35a3-4c51-aaac-1689ba244ad6%2FUntitled.png?table=block&id=05380b3d-e690-404f-8e62-9efd353d0c6f&spaceId=4ace8255-eeda-474a-8bb0-b4b4ff03eb62&width=860&userId=7c8d3a7d-0a81-4344-95b7-90711e3255dc&cache=v2"));
-            cellList2.add(createCell(CellType.TOGGLETEXTCELL, "2010년 연구에 따르면 18세에서 29세 사이의 사람들 중 무려 38%가 일생에 한번 이상 타투를 해본 경험이 있다고 합니다."));
+            cellList1 = createCells("타투 말은 많이 들어보고 주변에서도 요즘 많이 하던데 뭐가 뭔지 잘 모르겠어",
+                    "사람마다 모양, 색깔도 다르던데... 그리고 그거 아픈거 아냐?",
+                    "하는 당신을 위해 타투이스트 깜냥이가 모두 답변해드리겠습니닷!");
+
+            cellList2 = createCells("2010년 연구에 따르면 18세에서 29세 사이의 사람들 중 무려 38%가 일생에 한 번 이상 타투를 해본 경험이 있다고 합니다.",
+                    "약 10년이 넘게 지난 지금 타투에 관한 인식이 이전보다 많이 좋아졌기 때문에 훨씬 더 많은 사람이 타투를 해본 경험이 있을 것입니다.",
+                    "하지만 처음 타투를 하기로 다짐했다면 '타투를 하면 아프나요?'가 자연스러운 질문일 것입니다.");
 
             magazineList.add(createMagazine("타린이들을 위한 안내서", cellList1));
             magazineList.add(createMagazine("타투를 하면 얼마나 아픈가요?", cellList2));
@@ -139,9 +137,6 @@ public class InitDb {
             cellList2.forEach(cell -> cell.setMagazine(magazineList.get(1)));
 
             magazineList.forEach(em::persist);
-            cellList1.forEach(em::persist);
-            cellList2.forEach(em::persist);
-
         }
 
         private static User createUser(String id, String name, Role role, Address address) {
@@ -190,11 +185,60 @@ public class InitDb {
             return magazine;
         }
 
-        private static Cell createCell(CellType cellType, String content) {
+        private static Cell createCell(
+                CellType cellType, String text, Long fontSize,
+                TextColor textColor, TextAlignmentType textAlignment, FontWeightType fontWeight,
+                String imageUrlString, Long imageCornerRadius,
+                Long layoutHeight, Long layoutWidth,
+                Long layoutLeadingInset, Long layoutTrailingInset,
+                Long layoutTopInset, Long layoutBottomInset) {
             return Cell.builder()
                     .cellType(cellType)
-                    .content(content)
+                    .text(text)
+                    .fontSize(fontSize)
+                    .textColor(textColor)
+                    .textAlignment(textAlignment)
+                    .fontWeight(fontWeight)
+                    .imageUrlString(imageUrlString)
+                    .imageCornerRadius(imageCornerRadius)
+                    .layoutHeight(layoutHeight)
+                    .layoutWidth(layoutWidth)
+                    .layoutLeadingInset(layoutLeadingInset)
+                    .layoutTrailingInset(layoutTrailingInset)
+                    .layoutTopInset(layoutTopInset)
+                    .layoutBottomInset(layoutBottomInset)
                     .build();
+        }
+
+        private static List<Cell> createCells(String text1, String text2, String text3){
+            List<Cell> cellList = new ArrayList<>();
+            cellList.add(createCell(CellType.EMPTYCELL, null, 12L,
+                    TextColor.BLACK, TextAlignmentType.LEFT, FontWeightType.REGULAR,
+                    null, 0L, 10L,
+                    10L, 20L, 20L,
+                    20L, 20L));
+            cellList.add(createCell(CellType.IMAGECELL, null, 12L,
+                    TextColor.GRAY, TextAlignmentType.RIGHT, FontWeightType.MEDIUM,
+                    "https://이미지링크", 0L, 10L,
+                    10L, 20L, 20L,
+                    20L, 20L));
+            cellList.add(createCell(CellType.TEXTCELL, "타투 말은 많이 들어보고 주변에서도 요즘 많이 하던데 뭐가 뭔지 잘 모르겠어", 10L,
+                    TextColor.WHITE, TextAlignmentType.NATURAL, FontWeightType.BOLD,
+                    null, 0L, 10L,
+                    10L, 20L, 20L,
+                    20L, 20L));
+            cellList.add(createCell(CellType.TEXTCELL, "사람마다 모양, 색깔도 다르던데... 그리고 그거 아픈거 아냐?", 10L,
+                    TextColor.WHITE, TextAlignmentType.NATURAL, FontWeightType.BOLD,
+                    null, 0L, 10L,
+                    10L, 20L, 20L,
+                    20L, 20L));
+            cellList.add(createCell(CellType.TEXTCELL, "하는 당신을 위해 타투이스트 깜냥이가 모두 답변해드리겠습니닷!", 10L,
+                    TextColor.WHITE, TextAlignmentType.NATURAL, FontWeightType.BOLD,
+                    null, 0L, 10L,
+                    10L, 20L, 20L,
+                    20L, 20L));
+
+            return cellList;
         }
     }
 }

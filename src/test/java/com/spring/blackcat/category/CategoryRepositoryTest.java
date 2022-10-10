73fe0f349908f08bb72b52416724fa3bb0660b4b
roats <kimjinho123@naver.com>
2022-10-10 22:35:22 +0900

    Test: Add test display name

--- src/test/java/com/spring/blackcat/category/CategoryRepositoryTest.java
+++ src/test/java/com/spring/blackcat/category/CategoryRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.spring.blackcat.category;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +18,7 @@ class CategoryRepositoryTest {
     CategoryRepository categoryRepository;
 
     @Test
+    @DisplayName("ID로 카테고리 조회")
     void findById() {
         // given
         Category category = new Category("레터링", "Admin", "Admin");
@@ -30,6 +32,7 @@ class CategoryRepositoryTest {
     }
 
     @Test
+    @DisplayName("이름으로 카테고리 조회")
     void findByName() {
         // given
         Category category = new Category("레터링", "Admin", "Admin");
@@ -43,6 +46,7 @@ class CategoryRepositoryTest {
     }
 
     @Test
+    @DisplayName("전체 카테고리 조회")
     void findAll() {
         // given
         Category category1 = new Category("레터링", "Admin", "Admin");
@@ -63,6 +67,7 @@ class CategoryRepositoryTest {
     }
 
     @Test
+    @DisplayName("ID로 카테고리 삭제")
     void deleteByID() {
         // given
         Category category = new Category("레터링", "Admin", "Admin");
@@ -77,6 +82,7 @@ class CategoryRepositoryTest {
     }
 
     @Test
+    @DisplayName("전체 카테고리 삭제")
     void deleteAll() {
         // given
         Category category1 = new Category("레터링", "Admin", "Admin");

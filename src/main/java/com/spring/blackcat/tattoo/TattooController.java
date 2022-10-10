97ea47a2bbdf5df669c350a7d309ef4abfb9f5dc
AlwaysLee <54054460+AlwaysLee-12@users.noreply.github.com>
2022-10-10 22:37:36 +0900

    Feat/tattos (#6)
    * Chore: Add columns to tattoo domain
    
    * Feat: Add get all tattoos API
    
    * Fix: Set default pageable object
    
    * Chore: Add tattoo datas
    
    * Test: Add get all tattoos service test code
    
    * Fix: Fix conflict
    
    * Test: Add get all tattoos service test code for insert all query string case
    
    * Feat: Add get tattoos by category id API
    
    * Test: Add get tattoos by category id service test code
    
    * Style: Remove unnecessary code
    
    * Fix: Fix CI workflow
    
    * Fix: Fix isUserLikedTattoo method in TattooServiceImpl
    
    * Fix: Fix tattoo API

---
+++ src/main/java/com/spring/blackcat/tattoo/TattooController.java
@@ -0,0 +1,28 @@
+package com.spring.blackcat.tattoo;
+
+import com.spring.blackcat.tattoo.dto.GetTattoosResDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//path variable userId 수정 필요, request url userId 수정  필요)
+@RequiredArgsConstructor
+@RestController
+public class TattooController {
+    private final TattooService tattooService;
+
+    @GetMapping("/tattoos/{userId}")
+    public Page<GetTattoosResDto> getAllTattoos(@PageableDefault(page = 0, size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable, @PathVariable("userId") String userId) {
+        return this.tattooService.getAllTattoos(pageable, userId);
+    }
+
+    @GetMapping("tattoos/{userId}/categories/{categoryId}")
+    public Page<GetTattoosResDto> getTattoosByCategoryId(@PageableDefault(page = 0, size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable, @PathVariable("userId") String userId, @PathVariable("categoryId") Long categoryId) {
+        return this.tattooService.getTattoosByCategoryId(pageable, userId, categoryId);
+    }
+}
---
+++ src/test/java/com/spring/blackcat/tattoo/GetAllTattoosTest.java
@@ -0,0 +1,107 @@
+package com.spring.blackcat.tattoo;
+
+import com.spring.blackcat.category.CategoryRepository;
+import com.spring.blackcat.code.TattooType;
+import com.spring.blackcat.tattoo.dto.GetTattoosResDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class GetAllTattoosTest {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    TattooRepository tattooRepository;
+    @Autowired
+    TattooService tattooService;
+
+    @Test
+    @DisplayName("쿼리 스트링 없을 때 전체 타투 조회")
+    void getAllTattoosNoQueryString() {
+        //given
+        String userName = "Admin1";
+        //default pageable object
+        PageRequest pageRequest = PageRequest.of(0, 20, Sort.Direction.DESC, "id");
+
+        //when
+        List<GetTattoosResDto> allTattoos = tattooService.getAllTattoos(pageRequest, userName);
+
+        //then
+        assertThat(allTattoos.size()).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("쿼리 스트링 페이지만 넣었을 때 전체 타투 조회")
+    void getAllTattoosOnlyPageQueryString() {
+        //given
+        String userName = "Admin1";
+        //default pageable object
+        PageRequest pageRequest = PageRequest.of(1, 20, Sort.Direction.DESC, "id");
+
+        //when
+        List<GetTattoosResDto> allTattoos = tattooService.getAllTattoos(pageRequest, userName);
+
+        //then
+        //기본 20개 씩 조회 시, 두 번째 페이지에는 데이터가 없음(총 데이터 20개)
+        assertThat(allTattoos.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("쿼리 스트링 사이즈만 넣었을 때 전체 타투 조회")
+    void getAllTattoosOnlySizeQueryString() {
+        //given
+        String userName = "Admin1";
+        //default pageable object
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.Direction.DESC, "id");
+
+        //when
+        List<GetTattoosResDto> allTattoos = tattooService.getAllTattoos(pageRequest, userName);
+
+        //then
+        assertThat(allTattoos.size()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("쿼리 스트링 페이지, 사이즈만 넣었을 때 전체 타투 조회")
+    void getAllTattoosOnlyPageAndSizeQueryString() {
+        //given
+        String userName = "Admin1";
+        //default pageable object
+        PageRequest pageRequest = PageRequest.of(1, 5, Sort.Direction.DESC, "id");
+
+        //when
+        List<GetTattoosResDto> allTattoos = tattooService.getAllTattoos(pageRequest, userName);
+
+        //then
+        assertThat(allTattoos.size()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("쿼리 스트링 모두 넣었을 때 전체 타투 조회")
+    void getAllTattoosAllQueryString() {
+        //given
+        String userName = "Admin1";
+        //default pageable object
+        PageRequest pageRequest = PageRequest.of(0, 21, Sort.Direction.ASC, "price");
+        Tattoo tattoo = new Tattoo("작품1", "설명근", 0L, 5, categoryRepository.findByName("이레즈미").orElse(null), TattooType.DESIGN, userName, userName);
+        tattooRepository.save(tattoo);
+
+        //when
+        List<GetTattoosResDto> allTattoos = tattooService.getAllTattoos(pageRequest, userName);
+
+        //then
+        assertThat(allTattoos.size()).isEqualTo(21);
+        assertThat(allTattoos.get(0).getPrice()).isEqualTo(0);
+    }
+}

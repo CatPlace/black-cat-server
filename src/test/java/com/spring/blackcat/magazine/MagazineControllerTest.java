b8f25699b0304bcbf8a1945adfadea590d87fd2d
Hyunjik Lee <jiggy0429@gmail.com>
2022-10-10 21:59:54 +0900

    Chore: Remove unnecessary line

--- src/test/java/com/spring/blackcat/magazine/MagazineControllerTest.java
+++ src/test/java/com/spring/blackcat/magazine/MagazineControllerTest.java
@@ -1,24 +1,19 @@
 package com.spring.blackcat.magazine;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class MagazineControllerTest {
@@ -28,6 +23,7 @@ class MagazineControllerTest {
 
     @Autowired
     MagazineRepository magazineRepository;
+
     @Test
     @DisplayName("전체 매거진 목록 조회")
     void getAllMagazineTest() throws Exception {
@@ -44,6 +40,7 @@ class MagazineControllerTest {
                 .andExpect(jsonPath("code").exists())
                 .andExpect(jsonPath("data").isArray());
     }
+
     @DisplayName("특정 매거진 조회")
     @Test
     void getSpecificMagazineTest() throws Exception {
@@ -52,7 +49,7 @@ class MagazineControllerTest {
         //when
 
         //then
-        mockMvc.perform(get("/magazines/"+ String.valueOf(id)))
+        mockMvc.perform(get("/magazines/" + String.valueOf(id)))
                 .andDo(print())
                 .andExpect(jsonPath("status").exists())
                 .andExpect(jsonPath("code").exists())
@@ -61,7 +58,7 @@ class MagazineControllerTest {
 
     @DisplayName("매거진 등록")
     @Test
-    void registerMagazineTest(){
+    void registerMagazineTest() {
         //given
 
         //when

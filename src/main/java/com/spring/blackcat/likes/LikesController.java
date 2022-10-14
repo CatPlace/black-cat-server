d3441ba96735a0f51a66b440cf57bd26836287ae
roats <kimjinho123@naver.com>
2022-10-14 22:23:10 +0900

    Refactor: Modify Request URL

--- src/main/java/com/spring/blackcat/likes/LikesController.java
+++ src/main/java/com/spring/blackcat/likes/LikesController.java
@@ -9,21 +9,24 @@ public class LikesController {
 
     private final LikesService likesService;
 
-    @GetMapping("/likes/{postId}/{userId}")
+    // TODO: userId 제거
+    @GetMapping("/user/{userId}/likes/post/{postId}")
     public LikesStatusDto isLikedThisPost(
             @PathVariable("postId") Long postId,
             @PathVariable("userId") String userId) {
         return likesService.isLikedThisPost(postId, userId);
     }
 
-    @PostMapping("/likes/{postId}/{userId}")
+    // TODO: userId 제거
+    @PostMapping("/user/{userId}/likes/post/{postId}")
     public LikesStatusDto likesOn(
             @PathVariable("postId") Long postId,
             @PathVariable("userId") String userId) {
         return likesService.likesOn(postId, userId);
     }
 
-    @DeleteMapping("/likes/{postId}/{userId}")
+    // TODO: userId 제거
+    @DeleteMapping("/user/{userId}/likes/post/{postId}")
     public LikesStatusDto likesOff(
             @PathVariable("postId") Long postId,
             @PathVariable("userId") String userId) {

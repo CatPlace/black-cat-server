486a957550278031e35c934d1e1b8e8c43321f22
Hyunjik Lee <jiggy0429@gmail.com>
2022-10-15 17:49:14 +0900

    Fix: Remove redundant line in MagazineRepository

--- src/main/java/com/spring/blackcat/magazine/MagazineRepository.java
+++ src/main/java/com/spring/blackcat/magazine/MagazineRepository.java
@@ -2,8 +2,5 @@ package com.spring.blackcat.magazine;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MagazineRepository extends JpaRepository<Magazine, Long> {
-    Optional<Magazine> findById(Long id);
 }

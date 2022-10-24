55d116c3e3ab562af4f8d5a99f65f20db5b29633
alwayslee <ddoddo1997@naver.com>
2022-10-24 20:59:22 +0900

    Fix: Fix category not found exception error code

--- src/main/java/com/spring/blackcat/common/exception/ErrorInfo.java
+++ src/main/java/com/spring/blackcat/common/exception/ErrorInfo.java
@@ -9,7 +9,7 @@ public enum ErrorInfo {
     MAGAZINE_NOT_FOUND_EXCEPTION(400, 1000, "존재하지 않는 매거진 입니다."),
     USER_NOT_FOUND_EXCEPTION(400, 1001, "존재하지 않는 사용자 입니다."),
     TATTOO_NOT_FOUND_EXCEPTION(400, 1002, "존재하지 않는 타투 입니다."),
-    CATEGORY_NOT_FOUND_EXCEPTION(400, 1002, "존재하지 않는 카테고리 입니다.");
+    CATEGORY_NOT_FOUND_EXCEPTION(400, 1003, "존재하지 않는 카테고리 입니다.");
 
     private int statusCode;
     private int errorCode;

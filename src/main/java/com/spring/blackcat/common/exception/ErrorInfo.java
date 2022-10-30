abdb9ff261b4f82b82c4c48eb4df7e2e82e06089
roats <kimjinho123@naver.com>
2022-10-30 22:17:12 +0900

    Refactor: Edit variable name

--- src/main/java/com/spring/blackcat/common/exception/ErrorInfo.java
+++ src/main/java/com/spring/blackcat/common/exception/ErrorInfo.java
@@ -13,7 +13,7 @@ public enum ErrorInfo {
     IMAGE_UPLOAD_FAILED(500, 1004, "이미지 업로드 실패"),
     INVALID_LOGIN_INPUT_EXCEPTION(400, 1005, "입력 값이 유효하지 않은 로그인 요청입니다."),
     INVALID_TOKEN_EXCEPTION(401, 1006, "유효하지 않은 토큰입니다."),
-    ILEGAL_TOKEN_EXCEPTION(401, 1007, "토큰이 없거나 타입이 잘못되었습니다."),
+    ILLEGAL_TOKEN_EXCEPTION(401, 1007, "토큰이 없거나 타입이 잘못되었습니다."),
     POST_TYPE_NOT_FOUND_EXCEPTION(400, 1008, "존재하지 않는 게시물 유형입니다.");
 
 

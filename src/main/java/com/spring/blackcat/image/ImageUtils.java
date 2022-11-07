a2359eec9565747b90193351fff1cad4b23323b5
Hyunjik Lee <jiggy0429@gmail.com>
2022-11-08 03:24:08 +0900

    Feat: Image Resizing

---
+++ src/main/java/com/spring/blackcat/image/ImageUtils.java
@@ -0,0 +1,31 @@
+package com.spring.blackcat.image;
+
+import com.spring.blackcat.common.exception.ErrorInfo;
+import com.spring.blackcat.common.exception.custom.ImageUploadFailedException;
+import org.imgscalr.Scalr;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageUtils {
+
+    public static BufferedImage resize(File file, int width, int height) {
+        BufferedImage inputImage = null;
+        try {
+            inputImage = ImageIO.read(file);
+        } catch (IOException e) {
+            throw new ImageUploadFailedException("이미지 리사이징 실패", ErrorInfo.IMAGE_UPLOAD_FAILED);
+        }
+
+        int imgWidth = Math.min(inputImage.getWidth(), inputImage.getHeight());
+        int imgHeight = imgWidth;
+
+        BufferedImage outputImage = Scalr.crop(inputImage, (inputImage.getWidth() - imgWidth) / 2, (inputImage.getHeight() - imgHeight) / 2, imgWidth, imgHeight, null);
+
+        outputImage = Scalr.resize(outputImage, Scalr.Method.BALANCED, width, height, null);
+
+        return outputImage;
+    }
+}

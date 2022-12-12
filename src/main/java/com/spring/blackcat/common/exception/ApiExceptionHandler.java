ef106a569e26ab0901dfb6ce327bd03d5ed2935f
alwayslee <ddoddo1997@naver.com>
2022-12-12 23:10:45 +0900

    Refactor: Add exception handler

--- src/main/java/com/spring/blackcat/common/exception/ApiExceptionHandler.java
+++ src/main/java/com/spring/blackcat/common/exception/ApiExceptionHandler.java
@@ -3,13 +3,13 @@ package com.spring.blackcat.common.exception;
 import com.spring.blackcat.common.exception.custom.CustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
-import static com.spring.blackcat.common.exception.ErrorInfo.NULL_INPUT_EXCEPTION;
-import static com.spring.blackcat.common.exception.ErrorInfo.UNKNOWN_EXCEPTION;
+import static com.spring.blackcat.common.exception.ErrorInfo.*;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
@@ -23,6 +23,12 @@ public class ApiExceptionHandler {
     @ExceptionHandler({MethodArgumentNotValidException.class, MissingServletRequestPartException.class})
     public ResponseEntity<ErrorResponse> methodValidException() {
         ErrorResponse errorResponse = new ErrorResponse(NULL_INPUT_EXCEPTION);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> notReadableException() {
+        ErrorResponse errorResponse = new ErrorResponse(INVALID_INPUT_EXCEPTION);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
